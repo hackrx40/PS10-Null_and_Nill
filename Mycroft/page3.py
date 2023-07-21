38cb8097cbaa14938e62998f2a47261b453c9638
Pranav Malakar <pranav.malakar@gmail.com>
2023-07-21 11:15:37 +0530

    OCR Integrated

--- Mycroft/page3.py
+++ Mycroft/page3.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pathlib
+import os
+import glob
+import requests
+import pdfplumber
 
 st.set_page_config(page_title="Null And Nill", page_icon=":double_curly_loop:", layout="wide")
 
@@ -16,6 +21,8 @@ with st.sidebar:
 	st.header("1. SHERLOCK")
 	st.header("2. WATSON")
 	st.header("3. MYCROFT")
+
+
 st.title("MYCROFT")
 st.text("Upload legal documents skimp through important details.")
 with st.form(key="Form :", clear_on_submit = True):
@@ -24,13 +31,17 @@ with st.form(key="Form :", clear_on_submit = True):
 
 if Submit :
 	st.markdown("**The file is sucessfully Uploaded.**")
-
-	# Save uploaded file to 'F:/tmp' folder.
-	save_folder = 'F:/tmp'
-	save_path = Path(save_folder, File.name)
+	save_folder = 'Uploaded_Data'
+	save_path = pathlib.Path(save_folder, File.name)
 	with open(save_path, mode='wb') as w:
 		w.write(File.getvalue())
-
 	if save_path.exists():
 		st.success(f'File {File.name} is successfully saved!')
-
+	st.text("Processing")
+	os.system(f'ocrmypdf {File.name} output.pdf --force-ocr')
+	data=[]
+	with pdfplumber.open('output.pdf') as pdf:
+    		for i in range(len(pdf.pages)):
+        		data.append(pdf.pages[i].extract_text(x_tolerance=2))
+	os.remove("output.pdf")
+	st.markdown(''.join(data))
