d9f47fff954b2733b6c8746512df3b93de59a7e6
Pranav Malakar <92641237+pranav-malakar@users.noreply.github.com>
2023-07-22 07:59:50 +0530

    summary added

--- Mycroft/page3.py
+++ Mycroft/page3.py
@@ -4,8 +4,48 @@ import os
 import glob
 import requests
 import pdfplumber
+import sys
+from streamlit_extras.switch_page_button import switch_page
+import openai
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.llms import OpenAI
+from langchain.vectorstores import Chroma
+
 
 st.set_page_config(page_title="Null And Nill", page_icon=":double_curly_loop:", layout="wide")
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+# Enable to save to disk & reuse the model (for repeated queries on the same data)
+PERSIST = False
+
+query = None
+if len(sys.argv) > 1:
+  query = sys.argv[1]
+
+if PERSIST and os.path.exists("persist"):
+  print("Reusing index...\n")
+  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+else:
+  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
+  loader = DirectoryLoader("/home/pranav/Desktop/data/")
+  if PERSIST:
+    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+  else:
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+chain = ConversationalRetrievalChain.from_llm(
+  llm=ChatOpenAI(model="gpt-3.5-turbo"),
+  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+)
+
+chat_history = []
 
 st.markdown("""
 <style>
@@ -19,8 +59,7 @@ st.markdown("""
 with st.sidebar:
 	st.title("Index")
 	st.header("1. SHERLOCK")
-	st.header("2. WATSON")
-	st.header("3. MYCROFT")
+	st.header("2. MYCROFT")
 
 
 st.title("MYCROFT")
@@ -37,11 +76,24 @@ if Submit :
 		w.write(File.getvalue())
 	if save_path.exists():
 		st.success(f'File {File.name} is successfully saved!')
-	st.text("Processing")
+	st.text("Processing...")
 	os.system(f'ocrmypdf {File.name} output.pdf --force-ocr')
 	data=[]
 	with pdfplumber.open('output.pdf') as pdf:
     		for i in range(len(pdf.pages)):
         		data.append(pdf.pages[i].extract_text(x_tolerance=2))
 	os.remove("output.pdf")
-	st.markdown(''.join(data))
+	#st.markdown(''.join(data))
+	
+	file1 = open("Uploaded_Data/Database.txt","a")
+	file1.writelines(data)
+	file1.close()
+	result = chain({"question": "What is the context of the document", "chat_history": chat_history})
+	st.markdown("The context of the document is-")
+	st.markdown(result['answer'])
+if st.button('Upload More Docs'):
+	pass
+
+#askme = st.button("Ask Me Anything")
+#if askme:
+    #switch_page("Contribute")
